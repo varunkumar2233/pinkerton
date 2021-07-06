a1cf63cd3df295dd26c53ce66a48f03b94d0de69
jitendra <jitendra.mail123@gmail.com>
2021-07-06 23:17:27 +0530

    cart page updated
    code committed

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { CartComponent } from '../cart/cart.component';
 
 @Component({
   selector: 'header',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  bsModalRef: BsModalRef | undefined;
+  
+  constructor(private modalService: BsModalService, router: Router) { }
 
   ngOnInit(): void {
   }
 
+ 
 }
+
+
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -10,8 +10,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { WrapperComponent } from './wrapper/wrapper.component';
 import { TermsConditionComponent } from './terms-condition/terms-condition.component';
-
-
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TabsetConfig, TabsModule } from 'ngx-bootstrap/tabs';
 @NgModule({
   declarations: [
      HeaderComponent,
@@ -23,9 +23,11 @@ import { TermsConditionComponent } from './terms-condition/terms-condition.compo
     CommonModule,
     MainRoutingModule,
     CollapseModule.forRoot(),
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    TabsModule.forRoot()
   ],
-  providers: [BsDropdownModule],
+  providers: [BsDropdownModule, TabsetConfig],
 
 })
 export class MainModule { }
